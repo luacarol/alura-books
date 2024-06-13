91cd3c96b4a047246ac65d96440e2b5cc4bc8048
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-13 18:36:00 -0300

    icone list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import Logo from './componentes/Logo';
+import perfil from './imagens/perfil.svg';
+import sacola from './imagens/sacola.svg';
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+const icones = [perfil, sacola]
 
 function App() {
   return (
@@ -11,6 +14,12 @@ function App() {
         <ul className='opcoes'>
           { textoOpcoes.map((textoOpcao) => (
             <li className='opcao'><p>{textoOpcao}</p></li>
+          )) }
+        </ul>
+
+        <ul className='icones'>
+          { icones.map((icone) => (
+            <li className='icone'><img src={icone} alt={`${icones} name`}/></li>
           )) }
         </ul>
       </header>
