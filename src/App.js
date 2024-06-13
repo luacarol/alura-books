3ec2de23bbfb5a4f7f79a6c367969cb68a8ea4bf
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-13 18:48:27 -0300

    updt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import IconesHeader from './componentes/IconesHeader';
-import Logo from './componentes/Logo';
-import OpcoesHeader from './componentes/OpcoesHeader';
+import Header from './componentes/Header';
 
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo/>
-        <OpcoesHeader/>
-        <IconesHeader/>
-      </header>
+      <Header/>
     </div>
   );
 }
