dd14a98dd71087fa6e073e2e059587abe73e9905
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-13 18:41:55 -0300

    opcoes header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import Logo from './componentes/Logo';
+import OpcoesHeader from './componentes/OpcoesHeader';
 import perfil from './imagens/perfil.svg';
 import sacola from './imagens/sacola.svg';
 
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 const icones = [perfil, sacola]
 
 function App() {
@@ -11,12 +11,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <Logo/>
-        <ul className='opcoes'>
-          { textoOpcoes.map((textoOpcao) => (
-            <li className='opcao'><p>{textoOpcao}</p></li>
-          )) }
-        </ul>
-
+        <OpcoesHeader/>
         <ul className='icones'>
           { icones.map((icone) => (
             <li className='icone'><img src={icone} alt={`${icones} name`}/></li>
---
+++ src/componentes/OpcoesHeader/index.js
@@ -0,0 +1,15 @@
+import './estilo.css';
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+
+function OpcoesHeader() {
+    return (
+        <ul className='opcoes'>
+          { textoOpcoes.map((textoOpcao) => (
+            <li className='opcao'><p>{textoOpcao}</p></li>
+          )) }
+        </ul>
+    )
+}
+
+export default OpcoesHeader;
