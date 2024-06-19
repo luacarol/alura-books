1b1deba34ec90732c788b8dafff711b5c40f9889
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-19 12:33:30 -0300

    opcoes styled components

--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,30 @@
-import './estilo.css';
+import styled from 'styled-components';
+
+const Opcoes = styled.ul`
+  display: flex;
+`
+
+const Opcao = styled.li`
+  font-size: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+  min-width: 120px;
+`
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
 
 function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
+        <Opcoes>
           { textoOpcoes.map((textoOpcao) => (
-            <li className='opcao'><p>{textoOpcao}</p></li>
+            <Opcao><p>{textoOpcao}</p></Opcao>
           )) }
-        </ul>
+        </Opcoes>
     )
 }
 
