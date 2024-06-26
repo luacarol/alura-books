ce6aa4bf91cfb060e18f9aa004861e0346ebdb33
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-26 18:13:53 -0300

    props

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,4 +1,5 @@
 import { livros } from './dadosUltimosLancamentos';
+import { Titulo } from '../Titulo/index';
 import styled from 'styled-components';
 
 const UltimosLancamentosContainer = styled.section`
@@ -6,16 +7,6 @@ const UltimosLancamentosContainer = styled.section`
     padding-bottom: 20px;
     display: flex;
     flex-direction: column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
