53594dc157763815d593ce0bb8f18dc27b441471
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-26 17:40:40 -0300

    filtering books

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,7 @@
 import Input from "../Input";
 import styled from 'styled-components';
 import { useState } from "react";
+import { livros } from './dadosPesquisa';
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -25,14 +26,26 @@ const Subtitulo = styled.h3`
 `
 
 function Pesquisa() {
-    const [textoDigitado, setTextoDigitado] = useState('')
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
 
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
             <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-            <Input placeholder="Escreva sua próxima leitura." onBlur={(e) => setTextoDigitado(e.target.value)} />
-            <p>{textoDigitado}</p>
+            <Input 
+                placeholder="Escreva sua próxima leitura." 
+                onBlur={e => {
+                    const textoDigitado = e.target.value 
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+                    setLivrosPesquisados(resultadoPesquisa)
+                }} 
+            />
+            {livrosPesquisados.map(livro => (
+                <div>
+                    <p>{livro.nome}</p>
+                    <img src={livro.src} alt={livro.id} />
+                </div>
+            ))}
         </PesquisaContainer>
     )
 }
