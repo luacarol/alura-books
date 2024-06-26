127b576f41d5dc379e2303cbd66f90e04ed5b5c3
Luana Caroliny <luana.oliveira@autaza.com>
2024-06-26 18:01:44 -0300

    styling

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -9,9 +9,9 @@ const UltimosLancamentosContainer = styled.section`
 `
 
 const Titulo = styled.h2`
-    width: 100%,
+    width: 100%;
     padding: 30px 0;
-    background-color: #FFF,
+    background-color: #FFF;
     color: #EB9B00;
     font-size: 36px;
     text-align: center;
