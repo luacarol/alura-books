953fa273389ff3a3260e592ae44d9b46cb0eda1c
Luana Caroliny <luana.oliveira@autaza.com>
2024-08-14 13:58:46 -0300

    get livros API

--- src/componentes/Pesquisa/dadosPesquisa.js
+++ src/componentes/Pesquisa/dadosPesquisa.js
@@ -5,4 +5,4 @@ export const livros = [
     {nome: 'JavaScript Assertivo', id: 2, src: livro},
     {nome: 'Cangaceiro em JavaScript', id: 3, src: livro},
     {nome: 'Aventureiros do Java', id: 4, src: livro}
-]
+]   
--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import Input from "../Input";
 import styled from 'styled-components';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { livros } from './dadosPesquisa';
+import { getLivros } from "../../servicos/livros";
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -27,6 +28,12 @@ const Subtitulo = styled.h3`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        const livrosAPI = getLivros()
+        setLivros(livrosAPI)
+    }, [livros])
 
     return (
         <PesquisaContainer>
